865df0db25bdb38aa88fbe4500a90b3bdfb49670
chenhaiwei <chenhaiweibooty@gmail.com>
2025-03-13 21:15:28 +0800

    detect over

--- app.py
+++ app.py
@@ -55,38 +55,15 @@ def behavior_detection():
 # 处理上传的视频
 @app.route("/upload_video", methods=["POST"])
 def upload_video():
-    global detection_boxes
-    detection_boxes = []  # 重置检测框数据
-
     video_file = request.files.get("video")
     if video_file:
         # 保存视频文件
         video_path = os.path.join(UPLOAD_FOLDER, video_file.filename)
-        # video_file.save(video_path)
+        video_file.save(video_path)
         # 将视频路径存储在会话中
         global current_video_path
         current_video_path = video_path
-        # # 使用 YOLOv8 模型进行检测
-        # cap = cv2.VideoCapture(video_path)
-        # start_time = time.time()
-        # while cap.isOpened():
-        #     success, frame = cap.read()
-        #     if not success:
-        #         break
 
-        #     # 调整输入图像的尺寸
-        #     frame = cv2.resize(frame, (640, 640))  # 调整输入图像的尺寸
-
-            # # 将帧转换为 GPU 张量
-            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # 转换为 RGB
-            # frame_tensor = torch.from_numpy(frame).permute(2, 0, 1).float().to("cuda") / 255.0  # 归一化并加载到 GPU
-            # frame_tensor = frame_tensor.unsqueeze(0)  # 添加 batch 维度
-
-        #     # 进行检测
-        #     results = model(frame_tensor)
-        #     detection_boxes = results[0].boxes.xyxy.cpu().numpy().tolist()  # 获取检测框坐标
-
-        # cap.release()
         return redirect(url_for("behavior_detection"))
     return "视频上传失败！"
 
@@ -103,8 +80,10 @@ def video_detection_feed():
         while is_detecting and cap.isOpened():
             success, frame = cap.read()
             if not success:
-                cap.set(cv2.CAP_PROP_POS_FRAMES, 0)  # 循环播放
-                continue
+                # 视频播放结束，停止检测
+                is_detecting = False
+                cap.release()
+                break
             # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # 转换为 RGB
             # frame_tensor = torch.from_numpy(frame).permute(2, 0, 1).float().to("cuda") / 255.0  # 归一化并加载到 GPU
             # frame_tensor = frame_tensor.unsqueeze(0)  # 添加 batch 维度
@@ -122,7 +101,7 @@ def video_detection_feed():
                    b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
         
         cap.release()
-
+        print("视频播放结束，检测已停止")
     return Response(generate(),
                    mimetype='multipart/x-mixed-replace; boundary=frame')
 # 实时视频流
@@ -155,11 +134,6 @@ def generate_frames():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-# 视频流路由
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 # 返回检测框数据
 @app.route('/detection_data')
